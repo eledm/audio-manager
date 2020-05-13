2140b2fb0350574a23881f4f2da54493a59e219b
eledm <elena@MacBook-Pro-de-Elena.local>
2020-05-13 14:43:39 +0200

    General Functions
    Nothing is working, need to develop idea.

--- AudioManager/Assets/Scripts/AudioItem.cs
+++ AudioManager/Assets/Scripts/AudioItem.cs
@@ -8,10 +8,17 @@ public class AudioItem
     public string name;
     public AudioClip myClip;
     [Range (0.0f,1.0f)]
-    public float volume;
+    public float volume = 1f;
     [HideInInspector]
     public AudioSource myAudioSource;
-    public bool isLooping;
-    public bool playOnAwake;
+    public bool isLooping = false;
+    public bool playOnAwake = false;
 
+    //AudioItem constructor
+    public AudioItem(string name, AudioClip audioClip, AudioSource audioSource)
+    {
+        this.name = name;
+        this.myClip = audioClip;
+        this.myAudioSource = audioSource;
+    }
 }
--- AudioManager/Assets/Scripts/AudioManager.cs
+++ AudioManager/Assets/Scripts/AudioManager.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
@@ -23,15 +25,33 @@ public class AudioManager : MonoBehaviour
         //initialize singleton instance
         _instance = this;
 
+
+        //assign the audio item settings to its audio source
+        audioItems = audioItems.Add FindObjectsOfType<AudioItem>();
+        AudioItem[] array;
+        
+        foreach (var sound in audioItems)
+        {
+            sound.myAudioSource = gameObject.AddComponent<AudioSource>();
+            sound.myAudioSource.clip = sound.myClip;
+            sound.myAudioSource.volume = sound.volume;
+            sound.myAudioSource.loop = sound.isLooping;
+            if (sound.playOnAwake == true)
+            {
+                sound.myAudioSource.Play();
+            }
+
+        }
+
     }
-
-    //audio sources where music will be played through
-    public AudioMixer audioMixer;
-    public AudioSource musicSource;
-    public AudioSource sfxSource;
-
+    
     //array of audioitems to play
     public AudioItem[] audioItems;
+    //reference to the AudioMixer
+    AudioMixer mixer = Resources.Load("Master") as AudioMixer;
+    //max and min pitch change levels
+    public static float maxPitch = 1.05f;
+    public static float minPitch = 0.95f;
 
     // Start is called before the first frame update
     void Start()
@@ -47,18 +67,45 @@ public class AudioManager : MonoBehaviour
 
     #region METHODS
 
-    //ONE PLAY METHOD, DEFAULT GOES TO MASTER, YOU CAN CHANGE THE OUTPUT TO MUSIC OR SFX)
+    //Play method: takes an AudioItem and the AudioMixerGroup you want to play it through
 
-    public void Play(AudioItem audioItem, AudioSource audioSource)
+    public static void Play(AudioItem audioItem, AudioMixerGroup bus)
     {
-        audioSource.clip = audioItem.myClip;
-        audioSource.Play();
+        audioItem.myAudioSource.clip = audioItem.myClip;
+        audioItem.myAudioSource.outputAudioMixerGroup = bus;
+        audioItem.myAudioSource.Play();
     }
-   
 
-    public void Stop()
+    //stops playing the selected AudioItem
+    public static void Stop(AudioItem audioItem)
     {
-        //loop through all the the audio sources and stop
+        if (audioItem.myAudioSource.isPlaying == true)
+            audioItem.myAudioSource.Stop();
+        else
+            Debug.Log("AudioItem " + audioItem.name + " is already stopped.");
+    }
+
+    //pauses the selected audio item
+    public static void Pause(AudioItem audioItem)
+    {
+        if (audioItem.myAudioSource.isPlaying == true)
+            audioItem.myAudioSource.Pause();
+        else
+            Debug.Log("AudioItem " + audioItem.name + " is already not playing.");
+    }   
+
+    //unpauses the selected audio item
+    public static void Unpause(AudioItem audioItem)
+    {
+        if (audioItem.myAudioSource.isPlaying == false)
+            audioItem.myAudioSource.UnPause();
+        else
+            Debug.Log("AudioItem " + audioItem.name + "is already playing.");
+    }
+
+    //loop through all the the audio sources and stop
+    public static void StopAll()
+    {
         var audioSources = FindObjectsOfType<AudioSource>();
         foreach (AudioSource audio in audioSources)
         {
@@ -66,9 +113,9 @@ public class AudioManager : MonoBehaviour
         }
     }
 
-    public void Pause()
+    //loop through all audio sources and pause
+    public static void PauseAll()
     {
-        //loop through all audio sources and pause
         var audioSources = FindObjectsOfType<AudioSource>();
         foreach (AudioSource audio in audioSources)
         {
@@ -76,9 +123,22 @@ public class AudioManager : MonoBehaviour
         }
     }
 
-    public void RandomizePitch()
+    //unpauses all audiosources
+    public static void UnpauseAll()
     {
+        var audioSources = FindObjectsOfType<AudioSource>();
+        foreach (AudioSource audio in audioSources)
+        {
+            audio.UnPause();
+        }
+    }
 
+
+    public static void PlayRandomizePitch(AudioItem audio, AudioMixerGroup bus)
+    {
+        audio.myAudioSource.outputAudioMixerGroup = bus;
+        audio.myAudioSource.pitch = Random.Range(minPitch, maxPitch);
+        audio.myAudioSource.Play();
     }
 
     #endregion
